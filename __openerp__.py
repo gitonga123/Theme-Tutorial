d605323f89928c835e72b93ba0fc6669aa2225ae
daniel <daniel@imap.gmail.com>
2017-02-20 18:05:25 +0300

    Learning Odoo Themes

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+{
+    # Theme information
+    'name': "Theme Tutorial",
+    'description': """
+        Learning Odoo Theme Builder
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates
+    'data': [
+        'views/options.xml',
+        'views/snippets.xml',
+    ],
+
+    # demo pages
+    'demo': [
+        'demo/pages.xml',
+    ],
+
+    # Your information
+    'author': "Daniel Mutwiri",
+    'website': "www.otbafrica.com",
+}
